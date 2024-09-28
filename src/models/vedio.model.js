1482bfa0029c6fdee716131daa6d14cfce8e72b9
Anshuman Raghuwanshi <ar8839396153@gmail.com>
2024-09-28 14:29:12 +0530

    add user model and vedio model

---
+++ src/models/vedio.model.js
@@ -0,0 +1,43 @@
+import mongoose, {Schema} from 'mongoose';
+import mongooseAggregatePaginate from 'mongoose-aggregate-paginate-v2';
+const vedioSchema = new Schema (
+    {
+        vedioFile:{
+            type:String, //cloud url
+            required:true
+        },
+        thumbnail:{
+            type:String, //cloud url
+            required:true
+        },
+        title:{
+            type:String, 
+            required:true
+        },
+        discription:{
+            type:String, 
+            required:true
+        },
+        duration:{
+            type:Number, 
+            required:true
+        },
+        views:{
+            type:Number,
+            default:0
+        },
+        isPublished:{
+            type:Boolean,
+            default:true
+        },
+        owner:{
+            type:Schema.Type.ObjectId,
+            ref:"User"
+        }
+    },
+    {
+        timestamps:true
+    }
+)
+vedioSchema.plugin(mongooseAggregatePaginate)
+export const Vedio = mongoose.model("Vedio",vedioSchema)
