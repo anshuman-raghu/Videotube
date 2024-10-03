5d560b23d0bda66b673240a9eb0eaf0a18240e61
Anshuman Raghuwanshi <ar8839396153@gmail.com>
2024-10-03 16:46:28 +0530

    Backend Complete Now Testing Left

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import cors from 'cors'
 import multer from 'multer';
 
 const app = express()
+
 app.use(cors({
     origin:process.env.CORS_ORIGIN,
     credential: true
@@ -17,11 +18,29 @@ app.use(cookieParser())
 //Multer middleware
 const upload = multer();
 
+
 //Routes imports
 import userRoutes from "./routes/user.routes.js"
+import healthcheckRouter from "./routes/healthcheck.routes.js"
+import tweetRouter from "./routes/tweet.routes.js"
+import subscriptionRouter from "./routes/subscription.routes.js"
+import videoRouter from "./routes/video.routes.js"
+import commentRouter from "./routes/comment.routes.js"
+import likeRouter from "./routes/like.routes.js"
+import playlistRouter from "./routes/playlist.routes.js"
+import dashboardRouter from "./routes/dashboard.routes.js"
+
 
 // Router Declaration 
+app.use("/api/v1/healthcheck", healthcheckRouter)
 app.use("/api/v1/users", userRoutes)
+app.use("/api/v1/tweets", tweetRouter)
+app.use("/api/v1/subscriptions", subscriptionRouter)
+app.use("/api/v1/videos", videoRouter)
+app.use("/api/v1/comments", commentRouter)
+app.use("/api/v1/likes", likeRouter)
+app.use("/api/v1/playlist", playlistRouter)
+app.use("/api/v1/dashboard", dashboardRouter)
 
 
 
